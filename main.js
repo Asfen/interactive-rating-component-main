e35a13eea5274cd82342c4d73207a7889fbd1263
Asfen <Subin4ud@gmail.com>
2023-01-07 13:19:23 +0545

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// selecting elements that needs to be updated and saving them in a constant.
+const ratingdsp = document.querySelector('.submited-rating');
+const ratingPage = document.querySelector('.rating-page');
+const thankyouPage = document.querySelector('.thankyou-page');
+// adding event listener on submit button. Clicking will run submitClicked() function.
+document.getElementById('submit').addEventListener('click', submitClicked);
+// after submit is clicked this function runs. 
+//It first finds and saved checked radio button and saved it value or 0 if nothing checked to rating
+function submitClicked(){
+    const radioBtn = document.querySelector('input[name="rating"]:checked');
+    let rating = 0;
+    if(radioBtn) rating = radioBtn.value;
+    //rating selected msg is constructed and displayed.
+    ratingdsp.innerText = 'You selected ' + rating + ' out of 5';
+    // rating page becomes display:none (invisivle) and thankyou page becomes visible.
+    ratingPage.style.display = 'none';
+    thankyouPage.style.display = 'inline-block';
+}
+
+
